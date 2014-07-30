03f4ee371fad4adbb4cf7da094c27f085e285c35
Timothy Palpant <tim@palpant.us>
2014-07-29 22:25:41 -0400

    Initial Commit

---
+++ ConstraintSolver/ConstraintSolver.h
@@ -0,0 +1,19 @@
+//
+//  ConstraintSolver.h
+//  ConstraintSolver
+//
+//  Created by Timothy Palpant on 7/29/14.
+//  Copyright (c) 2014 Timothy Palpant. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ConstraintSolver.
+FOUNDATION_EXPORT double ConstraintSolverVersionNumber;
+
+//! Project version string for ConstraintSolver.
+FOUNDATION_EXPORT const unsigned char ConstraintSolverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConstraintSolver/PublicHeader.h>
+
+
