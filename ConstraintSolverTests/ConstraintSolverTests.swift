faef2b2087a4749d3326fac752b59e12355cbc68
Timothy Palpant <tim@palpant.us>
2015-01-05 23:46:08 -0500

    First simple tests with a single-variable problem

--- ConstraintSolverTests/ConstraintSolverTests.swift
+++ ConstraintSolverTests/ConstraintSolverTests.swift
@@ -6,31 +6,94 @@
 //  Copyright (c) 2014 Timothy Palpant. All rights reserved.
 //
 
-import Cocoa
 import XCTest
+import ConstraintSolver
 
 class ConstraintSolverTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  var s: Set<Int>!
+  var d: Domain<Int>!
+  var v: Variable<Int>!
+  var c: Constraint<Int>!
+  var p: Problem<Int>!
+  
+  override func setUp() {
+    super.setUp()
+    s = Set<Int>(1...10)
+    d = Domain<Int>(values: s)
+    v = Variable<Int>(domain: d)
+    c = MaxSumConstraint<Int>(variables: [v], sum: 4)
+    p = Problem<Int>()
+    p.variables.append(v)
+    p.constraints.append(c)
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testDomain() {
+    XCTAssertFalse(d.isEmpty, "Domain is not empty")
+    XCTAssertTrue(d.contains(4), "4 is in domain")
+    XCTAssertFalse(d.contains(-1), "-1 is not in domain")
+    XCTAssertFalse(d.contains(11), "11 is not in domain")
+    XCTAssertEqual(d.count, 10, "10 elements in domain")
+  }
+  
+  func testVariable() {
+    XCTAssertEqual(v.constraints.count, 1, "Variable has 1 constraint")
+    XCTAssert(v.assignment == nil, "Variable has no assignment")
+    v.assignment = 4
+    XCTAssertEqual(v.assignment!, 4, "Variable has assignment 4")
+  }
+  
+  func testConstraint() {
+    v.assignment = 3
+    XCTAssertTrue(c.evaluate(false), "Constraint is satisfied when v = 3")
+    v.assignment = 5
+    XCTAssertFalse(c.evaluate(false), "Constraint is not satisified when v = 5")
+    v.assignment = 1
+    XCTAssertTrue(c.evaluate(false), "Constraint is satisfied when v = 1")
+  }
+  
+  func testRecursiveBacktrackingSolver() {
+    let solver = RecursiveBacktrackingSolver()
+    solver.solve(p)
+    XCTAssertEqual(p.solutions.count, 4, "Problem has 4 solutions")
+    for sol in p.solutions {
+      for v in sol {
+        XCTAssertLessThanOrEqual(v, 4, "Solutions are <= 4")
+      }
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+  }
+  
+  func testBacktrackingSolver() {
+    let solver = BacktrackingSolver()
+    solver.solve(p)
+    XCTAssertEqual(p.solutions.count, 4, "Problem has 4 solutions")
+    for sol in p.solutions {
+      for v in sol {
+        XCTAssertLessThanOrEqual(v, 4, "Solutions are <= 4")
+      }
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+  }
+  
+  func testMinConflictsSolver() {
+    let solver = MinConflictsSolver()
+    solver.solve(p)
+    XCTAssertEqual(p.solutions.count, 1, "MinConflictsSolver only finds 1 solution")
+    for sol in p.solutions {
+      for v in sol {
+        XCTAssertLessThanOrEqual(v, 4, "Solution is <= 4")
+      }
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
+  //func testPerformanceExample() {
+    // This is an example of a performance test case.
+  //  self.measureBlock() {
+      // Put the code you want to measure the time of here.
+  //  }
+  //}
+  
 }
